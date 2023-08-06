8c0385e6935ccde792b0835b4c04e3689c9ac4a3
WillP90 <william.pearce000@gmail.com>
2023-08-06 11:49:44 -0700

    more code

--- flask_app/controllers/controller_profile.py
+++ flask_app/controllers/controller_profile.py
@@ -1,6 +1,22 @@
 from flask_app import app
 from flask import redirect, request, render_template, session
 from flask_app.models.model_user import User
+from flask_app.models.model_profile import Profile
+
+@app.route('/new/profile/<int:user_id>')
+def new_page(user_id):
+    return render_template('new_profile.html', user_id = user_id)
+
+@app.post('/new/profile/process')
+def process_profile():
+    data ={
+        "genre" : request.form['genre'],
+        "location" : request.form['location'],
+        "info" : request.form['info'],
+        "user_id" : request.form['user_id']
+    }
+    profile = Profile.save_user_profile(data)
+    return redirect('/profile')
 
 @app.route('/edit/profile/<int:user_id>')
 def edit_page(user_id):
