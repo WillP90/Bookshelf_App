7f79fdcc584657a536061bec372c1b1069e12e0c
WillP90 <william.pearce000@gmail.com>
2023-08-07 16:37:21 -0700

    another file

--- flask_app/controllers/controller_profile.py
+++ flask_app/controllers/controller_profile.py
@@ -3,9 +3,12 @@ from flask import redirect, request, render_template, session
 from flask_app.models.model_user import User
 from flask_app.models.model_profile import Profile
 
-@app.route('/new/profile/<int:user_id>')
-def new_page(user_id):
-    return render_template('new_profile.html', user_id = user_id)
+
+
+@app.route('/edit/profile/<int:user_id>')
+def edit_page(user_id):
+    return render_template('edit_profile.html', user_id = user_id)
+
 
 @app.post('/profile/process')
 def process_profile():
@@ -20,9 +23,3 @@ def process_profile():
         return redirect('/profile', profile = profile)
     profile = Profile.update_profile(data)
     return redirect('/profile', profile = profile)
-
-@app.route('/edit/profile/<int:user_id>')
-def edit_page(user_id):
-    # if Profile.get_one_profile_id(user_id) == False:
-    #     return render_template('edit_profile.html', user_id = user_id)
-    return render_template('new_profile.html', user_id = user_id)
