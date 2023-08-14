45435d0988800ffb64c61f3bfa4f1752fef2d2cd
WillP90 <william.pearce000@gmail.com>
2023-08-14 16:21:36 -0700

    more code

--- flask_app/controllers/controller_search.py
+++ flask_app/controllers/controller_search.py
@@ -1,11 +1,31 @@
 from flask_app import app
-from flask import redirect, request, render_template, session
-from flask_app.models.model_profile import Profile
+import requests
+from pprint import pprint
+from flask import redirect, render_template, request, session
 from flask_app.models.model_user import User
 from flask_app.models.model_book import Book
 
+
+
+# Helper Function
+def make_request(url ="https://www.googleapis.com/books/v1/volumes?q=search+terms"):
+    response = requests.get(url)
+    return response.json()
+
 @app.route('/search/book')
 def search_for_book():
-    if 'user_id' not in session:
-        return redirect('/logout')
+    key = 'AIzaSyBCxm0VWW3yC_yjqDCfkqIcj-bPozm3YNw'
+    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
+    print(str(response.status_code).center(20, "-"))
+    """ Get Request for Books to Google books API"""
+    # if not 'url' in session:
+    #     session['url'] = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+    # if 'user_id' not in session:
+    #     return redirect('/logout')
+
     return render_template('search_book.html')
+
+
+@app.post('/process/search')
+def search_name():
+    pass
